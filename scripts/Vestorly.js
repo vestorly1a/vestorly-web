3d62c01629d25060ffb8767b762c4cbddf5448b4
Alvin Lin <alvin.lin@stuypulse.com>
2015-08-08 12:51:40 -0400

    article querying

--- scripts/Vestorly.js
+++ scripts/Vestorly.js
@@ -9,13 +9,13 @@ function bind(object, method) {
   };
 }
 
-function Vestorly() {
-  this.sessionKey_ = null;
-};
+function Vestorly() {}
 
 Vestorly.USERNAME = encodeURIComponent('hackathon04@gmail.com');
 
 Vestorly.PASSWORD = encodeURIComponent('T4ZXmdYv');
+
+Vestorly.SESSION_KEY = null;
 
 Vestorly.CREDENTIALS = {
   username: Vestorly.USERNAME,
@@ -24,19 +24,36 @@ Vestorly.CREDENTIALS = {
 
 Vestorly.BASE_URL = 'https://staging.vestorly.com/api/v2';
 
-Vestorly.prototype.getSessionKey = function() {
-  var sessionKey = "blah";
-  async.series([
-    function(callback) {
-      $.post(Vestorly.BASE_URL + '/sessions?username=hackathon04%40gmail.com&password=T4ZXmdYv',
-            {},
-            function(response, status) {
-        callback(response['vestorly-auth']);
-      });
-    }], bind(this, function(data) {
-      this.sessionKey_ = data;
+Vestorly.getSessionKey = function() {
+  async.series(
+    [
+      function(callback) {
+        $.post(Vestorly.BASE_URL +
+               '/sessions?username=hackathon04%40gmail.com&password=T4ZXmdYv',
+               {},
+               function(response, status) {
+          Vestorly.SESSION_KEY = response['vestorly-auth'];
+        });
+      }
+    ], function(data) {
       return data;
-  }));
+    }
+  );
+};
+
+Vestorly.getArticle = function(number, query, callback) {
+  if (!Vestorly.SESSION_KEY) {
+    throw new Error('Session key was non initialized!');
+  }
+  $.get(Vestorly.BASE_URL + '/articles',
+        {
+          vestorly_auth: Vestorly.SESSION_KEY,
+          limit: 4,
+          text_query: 'python'
+        },
+        function(response, status) {
+    callback(response);
+  });
 };
 
 
