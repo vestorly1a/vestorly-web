5ffb4786c00f100fc861f4d986886358599e6113
Alvin Lin <alvin.lin@stuypulse.com>
2015-08-08 14:31:23 -0400

    parsing &nbsp;

---
+++ scripts/base.js
@@ -0,0 +1,9 @@
+/**
+ * Some base functions necessary for page functioning?
+ * Whatever I'm so tired. This project is so kludgy lol.
+ * @author Alvin Lin (alvin.lin@stuypulse.com)
+ */
+
+function parse(text) {
+  return text.replace("&nbsp;", " ");
+}
